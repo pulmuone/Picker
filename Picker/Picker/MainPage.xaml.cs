4d17ac51e770ed8a28ece87442a8243d38a7c0c9
gwise <pulmuone.ji@gmail.com>
2019-04-05 18:45:41 +0900

    Add project files.

---
+++ Picker/Picker/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Picker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Picker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysPageViewModel();
+        }
+    }
+}
